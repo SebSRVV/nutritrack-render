2633d3b9ea0f674ab613b703569549f8e5a94253
ale <u202116094@upc.edu.pe>
2025-11-21 00:08:00 -0500

    fix: conection to back end with render and optimization of goals module

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 export type Sex = 'female' | 'male';
 
